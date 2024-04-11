66ad1bc449dbc52ee6c0ad6d115fab14acaa4e0b
Scott Nath <github@scottnath.com>
2024-04-11 16:53:34 -0400

    :memo: title and text updates; schema fixes

--- workspaces/website/src/consts.ts
+++ workspaces/website/src/consts.ts
@@ -10,7 +10,7 @@ import mastodonLogo from '~/assets/logos/Mastodon.svg';
 export const SITE_TITLE = 'Scott Nath';
 export const SITE_SUBTITLE = 'Frontend Architect';
 export const ALT_TITLES = ['Solutions Architect', 'Software Architect', 'Full Stack Developer', 'DevOps Architect', 'Design Engineer', 'Senior Software Engineer', 'Design System Architect', 'Developer Advocate'];
-export const SCOTT_SHORT_DESC = 'Scott Nath is a web developer and actor living in New York State';
+export const SCOTT_SHORT_DESC = 'Scott Nath is a senior web developer and actor living in New York State';
 export const SITE_DESCRIPTION = `${SCOTT_SHORT_DESC} with extensive experience creating UI development systems with a focus on where developers and designers collaborate. Alt job titles: ${ALT_TITLES.join(', ')}`;
 
 
@@ -32,7 +32,11 @@ export const RESUME_DEFUALTS = {
 export const BLAHG = {
   title: `Blah blah blahg by Scott Nath`,
   description: `Articles and rambling-on by Scott Nath`,
+  pageTitle: '',
+  pageDesc: ''
 }
+BLAHG.pageTitle = `${BLAHG.title}: ${BLAHG.description}`
+BLAHG.pageDesc = `Technical blog posts featuring articles about Storybook, user interface testing, frontend web development, shared testing, and web components.`
 
 export const SOCIALS = [
   {
