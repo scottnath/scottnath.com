cdd3926335e4aae9641a224a0c9f1e557e1e1b9e
Scott Nath <github@scottnath.com>
2024-04-12 16:15:21 -0400

    :rocket: add pwd, lighthouse fixes

--- workspaces/website/astro.config.mjs
+++ workspaces/website/astro.config.mjs
@@ -3,11 +3,23 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
 import lit from '@astrojs/lit';
-
+import AstroPWA from '@vite-pwa/astro'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+    server: {
+      fs: {
+        // Allow serving files from hoisted root node_modules
+        allow: ['../..']
+      }
+    },
+  },
   devToolbar: {
     enabled: false
   },
@@ -21,6 +33,32 @@ export default defineConfig({
   },
 	site: 'https://scottnath.com',
 	integrations: [lit(), mdx(), sitemap(), react(), 
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Scott Nath PWA',
+        short_name: 'scottnath-pwa',
+        theme_color: '#0a0c10',
+      },
+      pwaAssets: {
+        config: true,
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      }
+    }),
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
