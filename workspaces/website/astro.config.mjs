466bf6cea384ec8ee45cfc3b6b761c351b173c93
Scott Nath <github@scottnath.com>
2024-04-12 17:01:12 -0400

    Update astro.config.mjs

--- workspaces/website/astro.config.mjs
+++ workspaces/website/astro.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
       },
       workbox: {
         navigateFallback: '/',
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,avif}'],
       },
       devOptions: {
         enabled: true,
