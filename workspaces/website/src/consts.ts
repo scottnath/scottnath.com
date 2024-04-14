58bd492d2e971b692de0fbb5916a7d26475d5a5b
Scott Nath <github@scottnath.com>
2024-04-14 18:20:42 -0400

    :memo: include blahg alt tags; shorter desc

--- workspaces/website/src/consts.ts
+++ workspaces/website/src/consts.ts
@@ -11,7 +11,7 @@ export const SITE_TITLE = 'Scott Nath';
 export const SITE_SUBTITLE = 'Frontend Architect';
 export const ALT_TITLES = ['Solutions Architect', 'Software Architect', 'Full Stack Developer', 'DevOps Architect', 'Design Engineer', 'Senior Software Engineer', 'Design System Architect', 'Developer Advocate'];
 export const SCOTT_SHORT_DESC = 'Scott Nath is a senior web developer and actor living in New York State';
-export const SITE_DESCRIPTION = `${SCOTT_SHORT_DESC} with extensive experience creating UI development systems with a focus on where developers and designers collaborate. Alt job titles: ${ALT_TITLES.join(', ')}`;
+export const SITE_DESCRIPTION = `${SCOTT_SHORT_DESC} with extensive experience creating full-stack apps and design systems.`;
 
 
 // image must be in root of public folder
