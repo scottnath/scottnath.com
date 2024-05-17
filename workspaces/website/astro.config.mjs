54dfbfde2d0ff92b734655a6564754c0d9a5ebf7
Scott Nath <github@scottnath.com>
2024-05-17 18:42:56 -0400

    :new: add jsonresume-component and article (#129)

--- workspaces/website/astro.config.mjs
+++ workspaces/website/astro.config.mjs
@@ -26,4 +26,10 @@ export default defineConfig({
       },
     }),
   ],
+  vite: {
+    ssr: {
+      // Example: Force a broken package to skip SSR processing, if needed
+      noExternal: ['jsonresume-component'],
+    }
+  }
 });
